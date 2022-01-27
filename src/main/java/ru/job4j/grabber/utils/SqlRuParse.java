fa19c06b270a3eecdeb26dcedaedbcce0c64b7ce
Olga_ <olga_art1025@mail.ru>
2022-01-27 22:20:09 +0500

    2.4. SqlRuParse [#285213 #255676] 3

--- src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
@@ -18,26 +18,40 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
-        Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            posts.add(detail(href.attr("href")));
+        try {
+            for (int i = 1; i < 6; i++) {
+                Document doc = Jsoup.connect(link + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Element href = td.child(0);
+                    posts.add(detail(href.attr("href")));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return posts;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        String title = doc.title().split("/ Вакансии")[0];
-        String description = doc.select(".msgBody").get(1).text();
-        LocalDateTime created = dateTimeParser
-                .parse(doc.select(".msgFooter")
-                        .get(0).text()
-                );
-        return new Post(title, link, description, created);
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            String title = doc.title().split("/ Вакансии")[0];
+            if (title.contains("java") && !title.contains("javascript")) {
+                String description = doc.select(".msgBody").get(1).text();
+                LocalDateTime created = dateTimeParser
+                        .parse(doc.select(".msgFooter")
+                                .get(0).text()
+                        );
+                post = new Post(title, link, description, created);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 }
