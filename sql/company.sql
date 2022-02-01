0d6c27af5ab408d110f8da4fb76ee3e1ca148a27
Olga_ <olga_art1025@mail.ru>
2022-02-01 22:19:20 +0500

    2. SQL Select by company [#2760]

---
+++ sql/company.sql
@@ -0,0 +1,32 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+SELECT c.id, c.name AS "Название компании", p.name AS "Ф.И.О"   
+FROM person AS p join company AS c
+ON p.company_id = c.id
+WHERE c.id != 5;
+
+SELECT c.name AS "Название компании",  COUNT(p.name)  AS "Количество человек"
+FROM person AS p join company AS c
+ON p.company_id = c.id
+GROUP BY c.name
+HAVING COUNT(p.name) =
+    (SELECT MAX (a.cnt) FROM
+		(SELECT COUNT(p.name) AS cnt FROM person AS p
+		JOIN company as c ON p.company_id = c.id               
+         GROUP BY (c.name)) AS a);		 
+		 
+		 
+		 
